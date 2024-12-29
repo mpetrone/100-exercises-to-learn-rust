4fbb5b5154a87a67dc699e5c3f4ae47f15b086db
mpetrone <7144280+mpetrone@users.noreply.github.com>
2024-12-29 17:37:27 +0100

    0602 exercise solution

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,14 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
 pub fn fibonacci(n: u32) -> u32 {
-    // TODO: implement the `fibonacci` function
-    //
-    // Hint: use a `Vec` to memoize the results you have already calculated
-    // so that you don't have to recalculate them several times.
-    todo!()
+    let mut numbers = vec![0, 1];
+    let number = n as usize;
+    for i in 2..=number {
+        numbers.push(numbers[i - 1] + numbers[i - 2])
+    }
+    numbers[number]
 }
 
 #[cfg(test)]
